273a3b66b09e1b65e3f96067107c3f5d9b24d2c8
dianaprude <diana19larina97@gmail.com>
2026-02-11 20:11:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from flask_cors import CORS
 from flask import Flask, request, jsonify
 from openai import OpenAI
 import os
 
 app = Flask(__name__)
+CORS(app)
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @app.route("/chat", methods=["POST"])
